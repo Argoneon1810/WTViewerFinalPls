323793aecdb44f96f5daa658fab2f9f364e55678
12193696 <62126566+Argoneon1810@users.noreply.github.com>
2022-06-09 00:37:28 +0900

    1. FragmentSettings의 최초 진입점 루트 취득 방식을 EditText에서 Spinner로 변경

--- app/src/main/java/ark/noah/wtviewerfinalpls/ui/settings/FragmentSettings.java
+++ app/src/main/java/ark/noah/wtviewerfinalpls/ui/settings/FragmentSettings.java
@@ -12,7 +12,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
+
+import java.lang.reflect.Array;
 
 import ark.noah.wtviewerfinalpls.DBHelper;
 import ark.noah.wtviewerfinalpls.EntryPointGetter;
@@ -37,19 +41,45 @@ public class FragmentSettings extends Fragment {
         SharedPreferences sharedPreferences = activityContext.getSharedPreferences(getString(R.string.shared_pref_key),
                 Context.MODE_PRIVATE);
 
-        binding.etxtSettingEntryUrl.setText(sharedPreferences.getString(activityContext.getString(R.string.shared_pref_entry_key), ""));
-
-        binding.btnSettingUrlApply.setOnClickListener(v -> {
-            String input = binding.etxtSettingEntryUrl.getText().toString();
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            if(EntryPointGetter.trySetEntryRoot(input)) {
-                Toast.makeText(activityContext.getApplicationContext(), activityContext.getText(R.string.notif_valid_entry_root), Toast.LENGTH_SHORT).show();
-                editor.putString(activityContext.getString(R.string.shared_pref_entry_key), input);
-            } else {
-                Toast.makeText(activityContext.getApplicationContext(), activityContext.getText(R.string.notif_valid_entry_root), Toast.LENGTH_SHORT).show();
-                editor.putString(activityContext.getString(R.string.shared_pref_entry_key), EntryPointGetter.EntryPointParser.defaultEntryRoot);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(activityContext,
+                R.layout.spinner_item, EntryPointGetter.EntryPointParser.validEntryRootList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.spinnerSettingUrl.setAdapter(adapter);
+        String lastEntryRoot = sharedPreferences.getString(activityContext.getString(R.string.shared_pref_entry_key), "");
+        if(lastEntryRoot.equals(""))
+            binding.spinnerSettingUrl.setSelection(6);
+        else {
+            for (int i = 0; i < adapter.getCount(); i++) {
+                if(adapter.getItem(i).equals(lastEntryRoot)) {
+                    binding.spinnerSettingUrl.setSelection(i);
+                }
             }
-            editor.apply();
+        }
+        binding.spinnerSettingUrl.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("", "Value Before Setting");
+                Log.i("", "entry point root: " + EntryPointGetter.EntryPointParser.getCurrentEntryRoot());
+                Log.i("", "entry point root in SharedPreference" + sharedPreferences.getString(activityContext.getString(R.string.shared_pref_entry_key), "FAILED"));
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                String input = (String) parent.getItemAtPosition(position);
+                if (EntryPointGetter.trySetEntryRoot(input)) {
+                    Toast.makeText(activityContext.getApplicationContext(), activityContext.getText(R.string.notif_valid_entry_root), Toast.LENGTH_SHORT).show();
+                    editor.putString(activityContext.getString(R.string.shared_pref_entry_key), input);
+                } else {
+                    Toast.makeText(activityContext.getApplicationContext(), activityContext.getText(R.string.notif_valid_entry_root), Toast.LENGTH_SHORT).show();
+                    editor.putString(activityContext.getString(R.string.shared_pref_entry_key), EntryPointGetter.EntryPointParser.defaultEntryRoot);
+                }
+                editor.apply();
+                Log.i("", "Value After Setting");
+                Log.i("", "entry point root: " + EntryPointGetter.EntryPointParser.getCurrentEntryRoot());
+                Log.i("", "entry point root in SharedPreference" + sharedPreferences.getString(activityContext.getString(R.string.shared_pref_entry_key), "FAILED"));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
         });
 
         return binding.getRoot();
